173a7d9744c3efb1bd4bb3d2987d0a3bde456788
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-08 21:33:15 +0300

    Add attachments to conftest.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from utilis import allure_attachments
+
 
 @pytest.fixture(autouse=True)
 def setup_browser():
@@ -14,3 +16,13 @@ def setup_browser():
 
     yield
     browser.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+    if result.when == 'call' and result.failed:
+        attach.attach_screenshot()
+        allure_attachments.attach_page_source()
+        allure_attachments.attach_logs()

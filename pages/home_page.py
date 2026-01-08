fcc67d1e387e26e1feee72721c4a514d2c1b40fe
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-08 19:18:50 +0300

    Add home_page.py

---
+++ pages/home_page.py
@@ -0,0 +1,31 @@
+import allure
+from selene import browser, by, be, have
+from pages.base_page import BasePage
+from pages.category_page import CategoryPage
+
+class HomePage(BasePage):
+    relative_url = "/"
+
+    @allure.step("Открыть главную страницу")
+    def open_home(self):
+        return self.open(self.relative_url)
+
+    @allure.step("Выполнить поиск по запросу: {query}")
+    def search(self, query: str):
+        browser.element('[name="search"]').type(query).press_enter()
+
+    @allure.step("Проверить, что результаты содержат {text}")
+    def should_see_result(self, text: str):
+        products = browser.all('.product-thumb')
+        products.should(have.size_at_least(1))
+        products.first().should(have.text(text))
+
+    @allure.step("Открыть меню Shop by Category")
+    def open_shop_by_category(self):
+        browser.element(by.text("Shop by Category")).should(be.clickable).click()
+        return self
+
+    @allure.step("Выбрать категорию {category} в Shop by Category")
+    def select_category(self, category: str) -> CategoryPage:
+        browser.element(f'//a[normalize-space()="{category}"]').should(be.clickable).click()
+        return CategoryPage()

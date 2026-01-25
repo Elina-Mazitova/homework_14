ece7b2afc56d1325caec0a6f72b2972b5386548c
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-25 20:34:40 +0300

    delete base_page.py

--- pages/base_page.py
+++
@@ -1,8 +0,0 @@
-from selene import browser
-
-class BasePage:
-    base_url = "https://ecommerce-playground.lambdatest.io"
-
-    def open(self, relative_url=""):
-        browser.open(self.base_url + relative_url)
-        return self

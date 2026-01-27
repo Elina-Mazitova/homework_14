b21adaa4664c11eb9a7347a4b7b08eb96659df0b
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-27 20:04:12 +0300

    removed version for local chrome

--- conftest.py
+++ conftest.py
@@ -19,22 +19,32 @@ load_dotenv()
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
+    selenoid_url = os.getenv("SELENOID_URL")
+
     # ==== Браузер и версия из .env ====
     browser_name = os.getenv("BROWSER_NAME", "chrome")
     browser_version = os.getenv("BROWSER_VERSION", "128.0")
 
     options = Options()
     options.set_capability("browserName", browser_name)
-    options.set_capability("browserVersion", browser_version)
-
-    # ==== Настройки Selenoid ====
-    selenoid_url = os.getenv("SELENOID_URL")
 
     if selenoid_url:
+        options.set_capability("browserVersion", browser_version)
+
+    # ==== Логи браузера ====
+    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
+
+    if selenoid_url:
+        options.set_capability("selenoid:options", {
+            "enableVNC": True,
+            "enableVideo": True
+        })
+
         selenoid_user = os.getenv("SELENOID_USER")
         selenoid_password = os.getenv("SELENOID_PASSWORD")
         selenoid_host = os.getenv("SELENOID_HOST", "selenoid.autotests.cloud")
 
+        # ClientConfig вместо user:pass@url
         client_config = ClientConfig(
             remote_server_addr=f"https://{selenoid_host}",
             username=selenoid_user,
@@ -54,30 +64,6 @@ def setup_browser():
     else:
         driver = webdriver.Chrome()
 
-    # ==== Логи браузера ====
-    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
-
-    # ==== Данные для Selenoid из .env ====
-    selenoid_user = os.getenv("SELENOID_USER")
-    selenoid_password = os.getenv("SELENOID_PASSWORD")
-    selenoid_host = os.getenv("SELENOID_HOST", "selenoid.autotests.cloud")
-
-    # ==== ClientConfig вместо user:pass@url ====
-    client_config = ClientConfig(
-        remote_server_addr=f"https://{selenoid_host}",
-        username=selenoid_user,
-        password=selenoid_password
-    )
-
-    remote_connection = RemoteConnection(
-        None,  # URL уже передан в client_config
-        client_config=client_config
-    )
-    driver = webdriver.Remote(
-        command_executor=remote_connection,
-        options=options
-    )
-
     # ==== Selene ====
     browser.config.driver = driver
     browser.config.base_url = os.getenv(
@@ -93,9 +79,12 @@ def setup_browser():
     attach_screenshot(driver)
     attach_page_source(driver)
     add_logs(browser)
-    attach_video(driver)
+
+    if selenoid_url:
+        attach_video(driver)
 
     driver.quit()
+
 
 @pytest.fixture
 def cart_cleaner():
